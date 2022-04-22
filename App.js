09714deee9dee4f875f30513f7f64b14c2efa93e
austinschroeder <austinsoundy@gmail.com>
2022-04-22 16:02:58 -0700

    Repo init

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Shaun');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>My name is {name}</Text>
+        <Text></Text>
+        <View style={styles.buttonContainer}>
+          <Button title="update state" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,5 +23,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
   },
 });
